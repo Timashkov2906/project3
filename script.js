e1c8b98deb27e217eb6d2a4dbf9fc0e06d0d30a7
Pavel <pav.timashkov@gmail.com>
2022-03-14 12:00:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+const numberOfFilms=prompt("Сколько фильмов вы уже посмотрели", '');
+const personalMovieDB  ={
+count :numberOfFilms,
+movies:{},
+actors:{},
+generas:[],
+privat:false
+};
+const a=prompt('один из последних просмотренных фильмов', ''),
+      b=prompt('На сколько оцените его?', ''),
+      c=prompt('один из последних просмотренных фильмов', ''),
+      d=prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
+
